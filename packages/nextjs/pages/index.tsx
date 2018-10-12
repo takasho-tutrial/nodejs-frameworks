2f3340adc8479ea836a71e5fb7f97d6e586124d5
taka-sho <takasho530000@gmail.com>
2018-10-13 01:52:25 +0900

    chore(package.json) tslint with prettier for nextjs

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 
 export default () =>
-  <div>
-    Hello World.{' '}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </div>
+  (
+    <div>
+      Hello World.{' '}
+      <Link href='/about'>
+        <a>About</a>
+      </Link>
+    </div>
+  )
